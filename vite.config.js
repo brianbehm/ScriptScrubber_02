e0ce37c8b9b15ad62768ae617a6ace9b73fddea0
Brian Behm <behm.brian@gmail.com>
2025-02-04 10:12:12 -0600

    Production-ready build

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    port: 3000
-  },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
